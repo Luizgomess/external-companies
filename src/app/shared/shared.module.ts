59c309634363284ecc5ff2f672dfe9df83b2858f
Luiz Gomes <luizgomes77777@gmail.com>
2024-06-23 21:46:39 -0300

    fix: inexistent import

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChangeCompanyComponent } from './components/modals/change-company/change-company.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { ModalContainerComponent } from '../modal-container.component';
 
 @NgModule({
   declarations: [
